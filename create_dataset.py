8ca81e28a6d1f7de9c57883af36b0ce5ea4431b1
hw@chill <hw56@indiana.edu>
2024-04-06 15:31:28 -0400

    feat: reduce val samples to 500; 5000 for testing

--- create_dataset.py
+++ create_dataset.py
@@ -2,7 +2,7 @@
 Create AUDIT-C Scoring dataset for a quick and dirty pilot run using
 `AUD_LLM_CX_04052024.csv`.
 
-The dataset was split in an 90/5/5 manner, randomly.
+206,864 samples are for training, 500 for val, and 5,000 for test.
 '''
 
 from datasets import load_dataset, DatasetDict
@@ -12,13 +12,13 @@ from utils import CSV_PATH, DATASET_PATH, SEED
 dataset = load_dataset('csv', data_files=CSV_PATH)
 dataset = dataset.shuffle(seed=SEED)
 
-train_test_split = dataset['train'].train_test_split(test_size=0.1, seed=SEED)
-val_test_split = train_test_split['test'].train_test_split(test_size=0.5, seed=SEED)
+train_test_split = dataset['train'].train_test_split(test_size=5500, seed=SEED)
+val_test_split = train_test_split['test'].train_test_split(test_size=5000, seed=SEED)
 
 split_datasets = DatasetDict({
-    'train': train_test_split['train'],  # 90%
-    'val': val_test_split['train'],  # 5%
-    'test': val_test_split['test']  # 5%
+    'train': train_test_split['train'],  # 206,864
+    'val': val_test_split['train'],  # 500
+    'test': val_test_split['test']  # 5,000
 })
 
 split_datasets.save_to_disk(DATASET_PATH)
