1cb963257282a4d59f0b65a99315eadda02821d6
hw@chill <hw56@indiana.edu>
2024-04-06 13:32:13 -0400

    feat: add utils

---
+++ utils.py
@@ -0,0 +1,9 @@
+
+
+CSV_PATH = 'gs://fc-secure-19ab668e-266f-4a5f-9c63-febea17b23cf/data/hw56/AUD_LLM_CX_04052024.csv'
+DATASET_PATH = 'gs://fc-secure-19ab668e-266f-4a5f-9c63-febea17b23cf/data/hw56/AUD_LLM_CX_04052024'
+MODEL_NAME = 'google/t5-base'
+PROJECT_NAME = 'AUDIT-C_LLMs'
+# RESPONSE_TEMP = ''
+SEED = 6179
+# TASK_PREFIX = ''
