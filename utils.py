b8b71b74efa3b3d7661e1ea5230fc4f077262c66
hw@chill <hw56@indiana.edu>
2024-04-06 14:39:49 -0400

    feat: change base to google/flan-t5-base

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@
 
 CSV_PATH = 'gs://fc-secure-19ab668e-266f-4a5f-9c63-febea17b23cf/data/hw56/AUD_LLM_CX_04052024.csv'
 DATASET_PATH = 'gs://fc-secure-19ab668e-266f-4a5f-9c63-febea17b23cf/data/hw56/AUD_LLM_CX_04052024'
-MODEL_NAME = 'google/t5-base'
+MODEL_NAME = 'google/flan-t5-base'
 PROJECT_NAME = 'AUDIT-C_LLMs'
 # RESPONSE_TEMP = ''
 SEED = 6179
