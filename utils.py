95533515b3b53c30f24739c3d689fe6c733ee2f3
hw@chill <hw56@indiana.edu>
2024-04-06 20:42:40 -0400

    feat: move const there

--- utils.py
+++ utils.py
@@ -4,7 +4,10 @@ CSV_PATH = 'gs://fc-secure-19ab668e-266f-4a5f-9c63-febea17b23cf/data/hw56/AUD_LL
 DATASET_PATH = 'gs://fc-secure-19ab668e-266f-4a5f-9c63-febea17b23cf/data/hw56/AUD_LLM_CX_04052024'
 MODEL_NAME = 'google/flan-t5-base'
 PROJECT_NAME = 'AUDIT-C_LLMs'
-MAX_OUTPUT_LENGTH = 4
-# RESPONSE_TEMP = ''
 SEED = 6179
-# TASK_PREFIX = ''
+
+MAX_LENGTH = 256
+MAX_OUTPUT_LENGTH = 8
+HEAD = ("### Score the user's Alcohol Use Disorders Identification Test (AUDIT-C) "
+        "from 0 to 12 based on the provided demographics and comorbidity data:\n")
+TAIL = "\n### AUDIT-C Score:"
