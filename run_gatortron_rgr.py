39d613cb03c0775d323db2f89b1532ad0331491e
hw@chill <hw56@indiana.edu>
2024-04-21 19:07:57 -0400

    refactorize: improve modularity

--- run_gatortron_rgr.py
+++ run_gatortron_rgr.py
@@ -5,24 +5,36 @@ virtually a single-token classification task). It has different input formats co
 to the T5 scripts.
 """
 
+import argparse
 import os
 
-import evaluate
 import torch
 import torch.nn as nn
 import wandb
-from datasets import load_from_disk, load_metric
-from torch.utils.data import DataLoader, Dataset
-from transformers import (AutoConfig, AutoModel,
-                          AutoModelForSequenceClassification, AutoTokenizer,
-                          BertPreTrainedModel, EarlyStoppingCallback, Trainer,
-                          TrainingArguments)
+from datasets import load_from_disk
+from torch.utils.data import Dataset
+from transformers import (
+    AutoConfig,
+    AutoModel,
+    AutoTokenizer,
+    BertPreTrainedModel,
+    EarlyStoppingCallback,
+    Trainer,
+    TrainingArguments,
+)
 
-from utils import (DATASET_PATH, PROJECT_NAME, convert_to_dataframe,
-                   expand_comorbidity)
+from utils import (
+    DATASET_PATH,
+    PROJECT_NAME,
+    SEED,
+    compute_metrics,
+    convert_to_dataframe,
+    expand_comorbidity,
+    period_separated_column_concatenation_formatting,
+)
 
 __author__ = "hw56@indiana.edu"
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 __license__ = "0BSD"
 
 
@@ -37,10 +49,10 @@ class GatorTron_Dataset(Dataset):
         self.df = df.reset_index(drop=True)  # drop index
         self.max_len = max_len
         self.texts = [
-            ", ".join(f"{k}: {v}" for k, v in row.items() if k != "audit.c.score")
-            for index, row in self.df.iterrows()
+            period_separated_column_concatenation_formatting(row)
+            for _, row in self.df.iterrows()
         ]
-        self.labels = df["audit.c.score"].tolist()
+        self.labels = [int(label) for label in df["audit.c.score"].tolist()]
 
         self.encodings = tokenizer(
             self.texts, truncation=True, padding="max_length", max_length=max_len
@@ -56,15 +68,17 @@ class GatorTron_Dataset(Dataset):
 
 
 class GatorTron_Regresser(BertPreTrainedModel):
-    def __init__(self, model_name):
+    def __init__(self, model_name, non_linear_head=False):
         config = AutoConfig.from_pretrained(model_name)
         super().__init__(config)
+        self.non_linear = non_linear_head
         self.gatortron = AutoModel.from_pretrained(model_name)
         self.cls_layer1 = nn.Linear(config.hidden_size, 128)
         self.relu1 = nn.ReLU()
-        # self.ff1 = nn.Linear(128, 128)
-        # self.tanh1 = nn.Tanh()
-        # self.dropout = nn.Dropout(config.hidden_dropout_prob)  # fixme: add dropout
+        if self.non_linear:
+            self.ff1 = nn.Linear(128, 128)
+            self.tanh1 = nn.Tanh()
+            self.dropout = nn.Dropout(config.hidden_dropout_prob)  # fixme: add dropout
         self.ff2 = nn.Linear(128, 1)
 
     def forward(self, input_ids, attention_mask, labels=None):
@@ -72,9 +86,10 @@ class GatorTron_Regresser(BertPreTrainedModel):
         logits = outputs.last_hidden_state[:, 0, :]
         output = self.cls_layer1(logits)
         output = self.relu1(output)
-        # output = self.ff1(output)
-        # output = self.tanh1(output)
-        # output = self.dropout(output)  # fixme: apply dropout
+        if self.non_linear_head:
+            output = self.ff1(output)
+            output = self.tanh1(output)
+            output = self.dropout(output)  # fixme: apply dropout
         output = self.ff2(output).squeeze(-1)
 
         loss = None
@@ -85,68 +100,72 @@ class GatorTron_Regresser(BertPreTrainedModel):
         return {"loss": loss, "logits": output}
 
 
-# tokenizer
-tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
-# metrics for regression
-mse_metric = evaluate.load("mse")
+if __name__ == "__main__":
+    torch.manual_seed(SEED + 676222)
+    parser = argparse.ArgumentParser(
+        description="Train a GatorTron regression model for AUDIT-C Scoring."
+    )
+    parser.add_argument(
+        "--non_linear_head",
+        type=bool,
+        default=False,
+        help="whether to use non-linear regression head",
+    )
+    args = parser.parse_args()
+    run_name = (
+        "gatrotron_rgr_linear_head"
+        if not args.non_linear_head
+        else "gatrotron_rgr_non_linear_head"
+    )
 
+    # load dataset and tokenizer
+    dataset = load_from_disk(DATASET_PATH)
+    tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
 
-def compute_metrics(eval_pred):
-    predictions, labels = eval_pred
-    mse = mse_metric.compute(predictions=predictions.squeeze(), references=labels)
-    return {"mse": mse}
+    train_df = expand_comorbidity(convert_to_dataframe(dataset["train"]))
+    val_df = expand_comorbidity(convert_to_dataframe(dataset["val"]))
+    test_df = expand_comorbidity(convert_to_dataframe(dataset["test"]))
 
+    train_dataset = GatorTron_Dataset(train_df, tokenizer, GATROTRON_MAX_LEN)
+    eval_dataset = GatorTron_Dataset(val_df, tokenizer, GATROTRON_MAX_LEN)
+    test_dataset = GatorTron_Dataset(test_df, tokenizer, GATROTRON_MAX_LEN)
 
-# load dataset
-dataset = load_from_disk(DATASET_PATH)
+    # init model
+    config = AutoConfig.from_pretrained(MODEL_NAME, num_labels=1)  # for regression
+    model = GatorTron_Regresser(MODEL_NAME).to(device)
 
-train_df = expand_comorbidity(convert_to_dataframe(dataset["train"]))
-val_df = expand_comorbidity(convert_to_dataframe(dataset["val"]))
-test_df = expand_comorbidity(convert_to_dataframe(dataset["test"]))
+    training_args = TrainingArguments(
+        output_dir=os.path.join("ckpts", run_name),
+        overwrite_output_dir=False,
+        num_train_epochs=10.0,
+        do_train=True,
+        do_eval=True,
+        do_predict=True,
+        evaluation_strategy="steps",
+        auto_find_batch_size=True,
+        gradient_accumulation_steps=4,
+        learning_rate=3e-4,
+        weight_decay=1e-1,
+        logging_steps=50,
+        eval_steps=100,
+        # bf16=True,
+        report_to="wandb",
+        load_best_model_at_end=True,
+        save_steps=100,
+        save_total_limit=3,
+        remove_unused_columns=True,
+    )
+    wandb.init(project=PROJECT_NAME, name=run_name, config=training_args)
 
-train_dataset = GatorTron_Dataset(train_df, tokenizer, GATROTRON_MAX_LEN)
-eval_dataset = GatorTron_Dataset(val_df, tokenizer, GATROTRON_MAX_LEN)
-test_dataset = GatorTron_Dataset(test_df, tokenizer, GATROTRON_MAX_LEN)
+    # initialize Trainer
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=train_dataset,
+        eval_dataset=eval_dataset,
+        compute_metrics=compute_metrics,
+        callbacks=[EarlyStoppingCallback(early_stopping_patience=20)],
+    )
 
-
-# init model
-config = AutoConfig.from_pretrained(MODEL_NAME, num_labels=1)  # for regression
-model = GatorTron_Regresser(MODEL_NAME).to(device)
-run_name = "gatrotron_rgr"
-
-training_args = TrainingArguments(
-    output_dir=f"ckpts/{run_name}",
-    overwrite_output_dir=False,
-    num_train_epochs=10.0,
-    do_train=True,
-    do_eval=True,
-    do_predict=True,
-    evaluation_strategy="steps",
-    auto_find_batch_size=True,
-    gradient_accumulation_steps=4,
-    learning_rate=3e-4,
-    weight_decay=1e-1,
-    logging_steps=50,
-    eval_steps=100,
-    # bf16=True,
-    report_to="wandb",
-    load_best_model_at_end=True,
-    save_steps=100,
-    save_total_limit=3,
-    remove_unused_columns=True,
-)
-wandb.init(project=PROJECT_NAME, name=run_name, config=training_args)
-
-
-# initialize Trainer
-trainer = Trainer(
-    model=model,
-    args=training_args,
-    train_dataset=train_dataset,
-    eval_dataset=eval_dataset,
-    compute_metrics=compute_metrics,
-    callbacks=[EarlyStoppingCallback(early_stopping_patience=20)],
-)
-
-# train the model
-trainer.train()
+    # train the model
+    trainer.train()
