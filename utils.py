1028ae8a914e7736f6a4079e19e9095cc9e8b5fa
hw@chill <hw56@indiana.edu>
2024-05-03 19:19:31 -0400

    feat: add new datafile CSV NAME

--- utils.py
+++ utils.py
@@ -3,7 +3,8 @@ import numpy as np
 from lifelines.utils import concordance_index
 from sklearn.metrics import mean_squared_error
 
-CSV_PATH = 'gs://fc-secure-19ab668e-266f-4a5f-9c63-febea17b23cf/data/hw56/AUD_LLM_CX_04052024.csv'
+CSV_DEMO_COMO_ONLY_PATH = 'gs://fc-secure-19ab668e-266f-4a5f-9c63-febea17b23cf/data/hw56/AUD_LLM_CX_04052024.csv'
+CSV_DEMO_COMO_THREE_DRUG_PATH = 'gs://fc-secure-19ab668e-266f-4a5f-9c63-febea17b23cf/data/hw56/AUD_LLM_Top3Drug_CX_05032024.csv'
 DATASET_PATH = 'gs://fc-secure-19ab668e-266f-4a5f-9c63-febea17b23cf/data/hw56/AUD_LLM_CX_04052024'
 MODEL_NAME = 'google/flan-t5-base'
 PROJECT_NAME = 'AUDIT-C_LLMs'
