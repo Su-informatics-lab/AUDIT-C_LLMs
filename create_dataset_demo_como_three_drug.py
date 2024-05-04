7708a12536c1161d55758797840f3206b8d36c19
hw@chill <hw56@indiana.edu>
2024-05-04 17:33:48 -0400

    feat: update scripts generating data for three_drup exp

--- create_dataset_demo_como_three_drug.py
+++ create_dataset_demo_como_three_drug.py
@@ -1,61 +1,67 @@
-'''
-Create AUDIT-C Scoring dataset for a quick and dirty pilot run using
-`AUD_LLM_Top3Drug_CX_05032024.csv`.
+"""Create the AUDIT-C Scoring dataset for a quick and dirty pilot run using both
+`AUD_LLM_CX_04052024.csv` (a wide-form table with demographics and comorbidity) and
+`AUD_LLM_Top3Drug_CX_05032024.csv` (a long-form table that has person_id from the pool
+of those in the wide-form who have at least one drug exposure record. Drug name
+(`concept_name`) and administration timestamp (`drug_exposure_start_date`) are reported.
 
-This dataset is a long-form
+The resulting dataframe contains the following columns:
+    - 'person_id', 'gender', 'race', 'ethnicity', 'age', 'comorbidity', 'q1.score',
+        'q2.score', 'q3.score', 'audit.c.score' (from the wide-form)
+    - 'concept_name_1', 'concept_name_2', 'concept_name_3',
+        'drug_exposure_start_date_1', 'drug_exposure_start_date_2',
+        'drug_exposure_start_date_3' (from the long-form)
+    - 'split' (indicating data split: 173,666 for train, 2,000 for validation, and 5,000
+        for test)
 
-111,569 samples are for training, 5,000 for val, and 5,000 for test.
-'''
+Note: The table has 65,837 null cells for comorbidity only.
+"""
 
-from datasets import load_dataset, DatasetDict
 import pandas as pd
-from utils import (CSV_DEMO_COMO_ONLY_PATH,
-                   CSV_THREE_DRUG_PATH,
-                   DEMO_COMO_THREE_DRUG_PARQUET_PATH,
-                   DEMO_COMO_THREE_DRUG_DATASET_PATH,
-                   SEED)
+from sklearn.model_selection import train_test_split
+
+from utils import (
+    CSV_DEMO_COMO_ONLY_PATH,
+    CSV_THREE_DRUG_PATH,
+    DEMO_COMO_THREE_DRUG_PARQUET_PATH,
+    SEED,
+)
 
 demo_como = pd.read_csv(CSV_DEMO_COMO_ONLY_PATH)
 drug = pd.read_csv(CSV_THREE_DRUG_PATH)
 # merge the dataframes based on person_id
-merged_df = pd.merge(demo_como, drug, on='person_id', how='left')
+merged_df = pd.merge(demo_como, drug, on="person_id", how="left")
 
 # create a sequential counter for each patient's drug exposure
-merged_df['exposure_counter'] = merged_df.groupby('person_id').cumcount().add(1)
+merged_df["exposure_counter"] = merged_df.groupby("person_id").cumcount().add(1)
 
 # pivot the long-form dataframe to wide-form
-pivoted_df = merged_df.pivot_table(
-    index=['person_id', 'gender', 'race', 'ethnicity', 'age', 'comorbidity',
-           'q1.score', 'q2.score', 'q3.score', 'audit.c.score'],
-    columns='exposure_counter',
-    values=['concept_name', 'drug_exposure_start_date'],
-    aggfunc='first')
+pivoted_drug_df = merged_df.pivot_table(
+    index="person_id",
+    columns="exposure_counter",
+    values=["concept_name", "drug_exposure_start_date"],
+    aggfunc="first",
+    fill_value="",
+)
 
 # flatten the multi-index columns
-pivoted_df.columns = [f'{col}_{num}' for col, num in pivoted_df.columns]
-pivoted_df.reset_index(inplace=True)
+pivoted_drug_df.columns = [f"{col}_{num}" for col, num in pivoted_drug_df.columns]
+pivoted_drug_df.reset_index(inplace=True)
 
 # merge the pivoted dataframe with the original wide-form dataframe
-final_df = pd.merge(demo_como, pivoted_df, on=['person_id', 'gender', 'race',
-                                               'ethnicity', 'age', 'comorbidity',
-                                               'q1.score', 'q2.score', 'q3.score',
-                                               'audit.c.score'], how='left')
-# filter out rows where the drug columns are null
-final_df = final_df.dropna(subset=[col for col in final_df.columns if 'concept_name' in col])
+# 180,666 (65837 null for comorbidity)
+final_df = pd.merge(demo_como, pivoted_drug_df, on=["person_id"], how="inner")
+
+# data split
+remaining_data, validation_data = train_test_split(
+    final_df, test_size=2000, random_state=SEED
+)
+remaining_data, test_data = train_test_split(
+    remaining_data, test_size=5000, random_state=SEED
+)
+validation_data["split"] = "validation"
+test_data["split"] = "test"
+remaining_data["split"] = "train"
+final_df_split = pd.concat([remaining_data, validation_data, test_data])
+
 # save parquet
-final_df.to_parquet(DEMO_COMO_THREE_DRUG_PARQUET_PATH, index=False)
-
-# read it back
-dataset = load_dataset('parquet', data_files=DEMO_COMO_THREE_DRUG_PARQUET_PATH)
-dataset = dataset.shuffle(seed=SEED)
-
-train_test_split = dataset['train'].train_test_split(test_size=5000 + 2000, seed=SEED)
-val_test_split = train_test_split['test'].train_test_split(test_size=5000, seed=SEED)
-
-split_datasets = DatasetDict({
-    'train': train_test_split['train'],  # 104,569
-    'val': val_test_split['train'],  # 2,000
-    'test': val_test_split['test']  # 5,000
-})
-
-split_datasets.save_to_disk(DEMO_COMO_THREE_DRUG_DATASET_PATH)
+final_df_split.to_parquet(DEMO_COMO_THREE_DRUG_PARQUET_PATH, index=False)
