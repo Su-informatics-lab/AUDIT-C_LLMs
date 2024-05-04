c3f3a77bd569142b596b2e3974381ae5e912b1ce
hw@chill <hw56@indiana.edu>
2024-05-03 21:24:55 -0400

    feat: migrate to parquet

--- utils.py
+++ utils.py
@@ -6,7 +6,7 @@ from sklearn.metrics import mean_squared_error
 CSV_DEMO_COMO_ONLY_PATH = 'gs://fc-secure-19ab668e-266f-4a5f-9c63-febea17b23cf/data/hw56/AUD_LLM_CX_04052024.csv'
 CSV_THREE_DRUG_PATH = 'gs://fc-secure-19ab668e-266f-4a5f-9c63-febea17b23cf/data/hw56/AUD_LLM_Top3Drug_CX_05032024.csv'
 DEMO_COMO_DATASET_PATH = 'gs://fc-secure-19ab668e-266f-4a5f-9c63-febea17b23cf/data/hw56/AUD_LLM_CX_04052024'
-DEMO_COMO_THREE_DRUG_CSV_PATH = 'gs://fc-secure-19ab668e-266f-4a5f-9c63-febea17b23cf/data/hw56/AUD_LLM_DEMO_COMO_THREE_DRUG_05032024.csv'
+DEMO_COMO_THREE_DRUG_PARQUET_PATH = 'gs://fc-secure-19ab668e-266f-4a5f-9c63-febea17b23cf/data/hw56/AUD_LLM_DEMO_COMO_THREE_DRUG_05032024.parquet'
 DEMO_COMO_THREE_DRUG_DATASET_PATH = 'gs://fc-secure-19ab668e-266f-4a5f-9c63-febea17b23cf/data/hw56/AUD_LLM_DEMO_COMO_THREE_DRUG_05032024'
 MODEL_NAME = 'google/flan-t5-base'
 PROJECT_NAME = 'AUDIT-C_LLMs'
