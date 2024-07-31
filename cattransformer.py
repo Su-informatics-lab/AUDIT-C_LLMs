7458a0c5b2b8eabf90802ddd619d66fb50128ea1
hw@chill <hw56@indiana.edu>
2024-07-30 20:11:06 -0400

    feat: lm/embed lookup on cpu

--- cattransformer.py
+++ cattransformer.py
@@ -29,6 +29,188 @@ from tqdm import tqdm
 from transformers import AutoModel, AutoTokenizer
 
 
+# class CatTransformer(nn.Module):
+#     def __init__(
+#             self,
+#             *,
+#             categories: list,  # a list containing the number of unique values (i.e., levels) within each easily encodable category (categories of low cardinality)
+#             num_high_card_categories: int,  # number of high-cardinality variables whose levels need to be derived from an LM; if set to 0, if falls back to a TabTransformer (https://arxiv.org/abs/2012.06678)
+#             num_continuous: int,  # number of continuous variables
+#             dim: int,  # input dimension/embedding size, paper set at 32
+#             depth: int = 6,  # number of stacking transformer blocks, paper recommended 6
+#             heads: int = 8,  # number of attention heads, paper recommends 8
+#             dim_head: int = 16,  # vector length for each attention head
+#             dim_out: int = 1,  # output dimension, fixed to 1 for regression
+#             mlp_hidden_mults: tuple = (4, 2),  # defines number of hidden layers of final MLP and multiplier of (bottom to top) input_size of (dim * num_categories) + num_continuous + dim
+#             mlp_act: nn.Module = nn.SiLU(),  # activation function for MLP
+#             continuous_mean_std: torch.Tensor = None,  # precomputed mean/std for continuous variables
+#             transformer_dropout: float = 0.1,  # dropout for attention and residual links
+#             use_shared_categ_embed: bool = True,  # share a fixed-length embeddings indicating the levels from the same column
+#             shared_categ_dim_divisor: int = 8,  # 1/8 of cat_embedding dims are shared in CatTransformer
+#             lm_model_name: str = 'distilbert/distilbert-base-uncased',  # Hugging Face BERT variant model name, and we recommend `Su-informatics-lab/gatortron_base_rxnorm_babbage_v2`
+#             lm_max_length: int = 512,  # max tokens for LM embedding computation
+#             embeddings_cache_path: str = '.lm_embeddings.pkl'  # path to cache embeddings
+#     ) -> None:
+#         super().__init__()
+#         assert all(map(lambda n: n > 0, categories)), 'number of each category must be positive'
+#         assert len(categories) + num_continuous + num_high_card_categories > 0, 'input shape must not be null'
+#
+#         self.num_categories = len(categories)
+#         self.num_high_card_categories = num_high_card_categories
+#         self.num_unique_categories = sum(categories)
+#         self.use_lm_embeddings = lm_model_name is not None and num_high_card_categories > 0
+#         self.dim = dim
+#         self.lm_max_length = lm_max_length
+#
+#         total_tokens = self.num_unique_categories + 1
+#         shared_embed_dim = 0 if not use_shared_categ_embed else int(dim // shared_categ_dim_divisor)
+#
+#         self.category_embed = nn.Embedding(total_tokens, dim - shared_embed_dim)
+#         self.use_shared_categ_embed = use_shared_categ_embed
+#
+#         if use_shared_categ_embed:
+#             self.shared_category_embed = nn.Parameter(torch.zeros(self.num_categories, shared_embed_dim))
+#             nn.init.normal_(self.shared_category_embed, std=0.02)
+#
+#         if self.num_unique_categories > 0:
+#             categories_offset = F.pad(torch.tensor(list(categories)), (1, 0), value=1)
+#             categories_offset = categories_offset.cumsum(dim=-1)[:-1]
+#             self.register_buffer('categories_offset', categories_offset)
+#
+#         self.num_continuous = num_continuous
+#
+#         if self.num_continuous > 0:
+#             if continuous_mean_std is not None:
+#                 assert continuous_mean_std.shape == (num_continuous, 2), (
+#                     f'continuous_mean_std must have a shape of ({num_continuous}, 2) where the last dimension contains the mean and variance respectively')
+#             self.register_buffer('continuous_mean_std', continuous_mean_std)
+#             self.cont_norm = nn.LayerNorm(num_continuous)
+#
+#         encoder_layers = TransformerEncoderLayer(d_model=dim, nhead=heads, dim_feedforward=dim * dim_head, dropout=transformer_dropout)
+#         self.transformer = TransformerEncoder(encoder_layers, num_layers=depth)
+#
+#         self.transformer_input_size = dim * (self.num_categories + self.num_high_card_categories)
+#         self.mlp_input_size = self.transformer_input_size + num_continuous
+#         self.hidden_dimensions = [self.mlp_input_size * t for t in mlp_hidden_mults]
+#         self.all_dimensions = [self.mlp_input_size, *self.hidden_dimensions, dim_out]
+#
+#         self.mlp = MLP(self.all_dimensions, act=mlp_act)
+#
+#         if self.use_lm_embeddings:
+#             self.lm_model, self.tokenizer = self.load_lm_model(lm_model_name)
+#             self.embeddings_cache_path = embeddings_cache_path
+#             self.embeddings_cache = self.load_embeddings_cache()
+#             self.projection_layer = nn.Linear(self.lm_model.config.hidden_size, self.dim)
+#
+#     def forward(self, x_categ: torch.Tensor, x_cont: torch.Tensor, x_high_card_categ: Optional[List[str]]) -> torch.Tensor:
+#         device = x_categ.device
+#         self.categories_offset = self.categories_offset.to(device)
+#
+#         assert x_categ.shape[1] == self.num_categories, f'You must pass in {self.num_categories} values for your categories input'
+#         assert x_cont.shape[1] == self.num_continuous, f'You must pass in {self.num_continuous} values for your continuous input'
+#
+#         x_categ = self.handle_missing_data(x_categ)
+#
+#         if self.num_unique_categories > 0:
+#             x_categ = x_categ + self.categories_offset
+#             categ_embed = self.category_embed(x_categ)
+#
+#             if self.use_shared_categ_embed:
+#                 shared_categ_embed = self.shared_category_embed.unsqueeze(0).repeat(categ_embed.shape[0], 1, 1)
+#                 shared_categ_embed = shared_categ_embed.to(device)
+#                 categ_embed = torch.cat((categ_embed, shared_categ_embed), dim=-1)
+#
+#             # only process high-cardinality features if they are present
+#             if self.num_high_card_categories > 0 and len(x_high_card_categ) > 0:
+#                 lm_cat_proj = self.get_lm_embeddings(x_high_card_categ, device)
+#                 lm_cat_proj = lm_cat_proj.view(x_categ.size(0), -1, self.dim)  # reshape to match batch size
+#                 categ_embed = torch.cat((categ_embed, lm_cat_proj), dim=1)
+#
+#         x = self.transformer(categ_embed)
+#         flat_categ = x.flatten(start_dim=1)
+#
+#         if self.num_continuous > 0:
+#             if self.continuous_mean_std is not None:
+#                 mean, std = self.continuous_mean_std.unbind(dim=-1)
+#                 x_cont = (x_cont - mean) / std
+#
+#             normed_cont = self.cont_norm(x_cont)
+#             x = torch.cat((flat_categ, normed_cont), dim=1)
+#         else:
+#             x = flat_categ
+#
+#         logits = self.mlp(x)
+#
+#         return logits
+#
+#     def handle_missing_data(self, x_categ: torch.Tensor) -> torch.Tensor:
+#         missing_data_mask = ((x_categ == 'na') | (x_categ == 'missing') |
+#                              (x_categ == np.nan) | (x_categ == 'NaN') |
+#                              (x_categ == 'N/A'))
+#         x_categ[missing_data_mask] = 0
+#         return x_categ
+#
+#     def load_lm_model(self, lm_model_name) -> tuple:
+#         model = AutoModel.from_pretrained(lm_model_name)
+#         tokenizer = AutoTokenizer.from_pretrained(lm_model_name)
+#         return model, tokenizer
+#
+#     def load_embeddings_cache(self):
+#         if os.path.exists(self.embeddings_cache_path):
+#             with open(self.embeddings_cache_path, 'rb') as f:
+#                 print(f"Loading cache from {self.embeddings_cache_path}.")
+#                 return pickle.load(f)
+#         print("Cache file not found. Initializing empty cache.")
+#         return {}
+#
+#     def save_embeddings_cache(self) -> None:
+#         with open(self.embeddings_cache_path, 'wb') as f:
+#             pickle.dump(self.embeddings_cache, f)
+#
+#     def get_lm_embeddings(self, x_high_card_categ: list,
+#                           device: torch.device) -> torch.Tensor:
+#         #
+#         new_texts = []
+#         for texts in x_high_card_categ:
+#             for text in texts:
+#                 if text not in self.embeddings_cache:
+#                     new_texts.append(text)
+#
+#         if new_texts:
+#             new_embeddings = self.compute_embeddings(new_texts, device)
+#             for text, embedding in zip(new_texts, new_embeddings):
+#                 self.embeddings_cache[text] = embedding
+#
+#             self.save_embeddings_cache()
+#
+#         embeddings = []
+#         for texts in x_high_card_categ:
+#             embedding_list = []
+#             for text in texts:
+#                 embedding_list.append(self.embeddings_cache[text].cpu())
+#             embeddings.append(np.stack(embedding_list, axis=0))
+#
+#         embeddings = np.stack(embeddings, axis=0)
+#         embeddings = torch.tensor(embeddings, dtype=torch.float32).to(device)
+#         embeddings = self.projection_layer(embeddings)
+#
+#         return embeddings
+#
+#     def compute_embeddings(self, texts: list, device: torch.device) -> torch.Tensor:
+#         embeddings = []
+#         for text in tqdm(texts, desc="Computing LM embeddings"):
+#             inputs = self.tokenizer(text, return_tensors='pt',
+#                                     truncation=True,
+#                                     padding=True,
+#                                     max_length=self.lm_max_length).to(device)
+#             outputs = self.lm_model(**inputs)
+#             cls_embedding = outputs.last_hidden_state[:, 0, :].detach().cpu().numpy()
+#             embeddings.append(cls_embedding)
+#
+#         embeddings = np.vstack(embeddings)
+#         return torch.tensor(embeddings, dtype=torch.float32).to(device)
+
+
 class CatTransformer(nn.Module):
     def __init__(
             self,
@@ -97,7 +279,8 @@ class CatTransformer(nn.Module):
         self.mlp = MLP(self.all_dimensions, act=mlp_act)
 
         if self.use_lm_embeddings:
-            self.lm_model, self.tokenizer = self.load_lm_model(lm_model_name)
+            # Load the LM model and tokenizer on CPU
+            self.lm_model, self.tokenizer = self.load_lm_model(lm_model_name, use_cuda=False)
             self.embeddings_cache_path = embeddings_cache_path
             self.embeddings_cache = self.load_embeddings_cache()
             self.projection_layer = nn.Linear(self.lm_model.config.hidden_size, self.dim)
@@ -113,20 +296,19 @@ class CatTransformer(nn.Module):
 
         if self.num_unique_categories > 0:
             x_categ = x_categ + self.categories_offset
-            categ_embed = self.category_embed(x_categ)
+            categ_embed = self.category_embed(x_categ).cpu()  # keep embeddings on RAM
 
             if self.use_shared_categ_embed:
                 shared_categ_embed = self.shared_category_embed.unsqueeze(0).repeat(categ_embed.shape[0], 1, 1)
-                shared_categ_embed = shared_categ_embed.to(device)
+                shared_categ_embed = shared_categ_embed.cpu()  # ensure shared embeddings are on CPU
                 categ_embed = torch.cat((categ_embed, shared_categ_embed), dim=-1)
 
-            # only process high-cardinality features if they are present
             if self.num_high_card_categories > 0 and len(x_high_card_categ) > 0:
                 lm_cat_proj = self.get_lm_embeddings(x_high_card_categ, device)
                 lm_cat_proj = lm_cat_proj.view(x_categ.size(0), -1, self.dim)  # reshape to match batch size
                 categ_embed = torch.cat((categ_embed, lm_cat_proj), dim=1)
 
-        x = self.transformer(categ_embed)
+        x = self.transformer(categ_embed.to(device))  # move to GPU for transformer computation
         flat_categ = x.flatten(start_dim=1)
 
         if self.num_continuous > 0:
@@ -150,9 +332,11 @@ class CatTransformer(nn.Module):
         x_categ[missing_data_mask] = 0
         return x_categ
 
-    def load_lm_model(self, lm_model_name) -> tuple:
+    def load_lm_model(self, lm_model_name, use_cuda=True) -> tuple:
         model = AutoModel.from_pretrained(lm_model_name)
         tokenizer = AutoTokenizer.from_pretrained(lm_model_name)
+        if use_cuda:
+            model = model.cuda()
         return model, tokenizer
 
     def load_embeddings_cache(self):
@@ -167,9 +351,7 @@ class CatTransformer(nn.Module):
         with open(self.embeddings_cache_path, 'wb') as f:
             pickle.dump(self.embeddings_cache, f)
 
-    def get_lm_embeddings(self, x_high_card_categ: list,
-                          device: torch.device) -> torch.Tensor:
-        #
+    def get_lm_embeddings(self, x_high_card_categ: list, device: torch.device) -> torch.Tensor:
         new_texts = []
         for texts in x_high_card_categ:
             for text in texts:
@@ -179,7 +361,7 @@ class CatTransformer(nn.Module):
         if new_texts:
             new_embeddings = self.compute_embeddings(new_texts, device)
             for text, embedding in zip(new_texts, new_embeddings):
-                self.embeddings_cache[text] = embedding
+                self.embeddings_cache[text] = embedding.cpu()  # store embeddings on RAM
 
             self.save_embeddings_cache()
 
@@ -196,68 +378,6 @@ class CatTransformer(nn.Module):
 
         return embeddings
 
-    # def get_lm_embeddings(self, x_high_card_categ: list,
-    #                       device: torch.device) -> torch.Tensor:
-    #     new_texts = []
-    #     for texts in x_high_card_categ:
-    #         for text in texts:
-    #             if text not in self.embeddings_cache:
-    #                 new_texts.append(text)
-    #
-    #     if new_texts:
-    #         new_embeddings = self.compute_embeddings(new_texts, device)
-    #         for text, embedding in zip(new_texts, new_embeddings):
-    #             self.embeddings_cache[text] = embedding
-    #
-    #         self.save_embeddings_cache()
-    #
-    #     embeddings = []
-    #     for texts in x_high_card_categ:
-    #         embedding_list = [self.embeddings_cache[text].cpu() for text in texts]
-    #         embeddings.append(np.stack(embedding_list, axis=0))
-    #     embeddings = np.stack(embeddings, axis=0)
-    #     embeddings = torch.tensor(embeddings, dtype=torch.float32).to(device)
-    #     # apply projection to reduce dimensionality
-    #     embeddings = self.projection_layer(embeddings)
-    #     return embeddings
-
-    # def get_lm_embeddings(self, x_high_card_categ: list,
-    #                       device: torch.device) -> torch.Tensor:
-    #     new_texts = []
-    #     for texts in x_high_card_categ:
-    #         for text in texts:
-    #             if text not in self.embeddings_cache:
-    #                 new_texts.append(text)
-    #
-    #     if new_texts:
-    #         print(f"New texts to compute embeddings for: {new_texts}")
-    #         new_embeddings = self.compute_embeddings(new_texts, device)
-    #         for text, embedding in zip(new_texts, new_embeddings):
-    #             self.embeddings_cache[text] = embedding
-    #
-    #         self.save_embeddings_cache()
-    #
-    #     embeddings = []
-    #     for texts in x_high_card_categ:
-    #         embedding_list = []
-    #         for text in texts:
-    #             if text in self.embeddings_cache:
-    #                 embedding_list.append(self.embeddings_cache[text].cpu())
-    #             else:
-    #                 print(f"Cache miss for text: {text}")
-    #         embeddings.append(np.stack(embedding_list, axis=0))
-    #
-    #     embeddings = np.stack(embeddings, axis=0)
-    #     embeddings = torch.tensor(embeddings, dtype=torch.float32).to(device)
-    #
-    #     print(f"Shape before projection: {embeddings.shape}")
-    #
-    #     embeddings = self.projection_layer(embeddings)
-    #
-    #     print(f"Shape after projection: {embeddings.shape}")
-    #
-    #     return embeddings
-
     def compute_embeddings(self, texts: list, device: torch.device) -> torch.Tensor:
         embeddings = []
         for text in tqdm(texts, desc="Computing LM embeddings"):
@@ -270,7 +390,7 @@ class CatTransformer(nn.Module):
             embeddings.append(cls_embedding)
 
         embeddings = np.vstack(embeddings)
-        return torch.tensor(embeddings, dtype=torch.float32).to(device)
+        return torch.tensor(embeddings, dtype=torch.float32).cpu()  # Keep on CPU
 
 
 class MLP(nn.Module):
